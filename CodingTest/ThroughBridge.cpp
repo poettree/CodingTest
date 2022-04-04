f7bfbf15316c017f01605d17d01377ac964e96ec
poettree <leeh3535@naver.com>
2022-04-04 12:58:05 +0900

    20220404(Fixing)Through_Bridge_Truck
    다리의 길이만큼 배열을 생성하는것까지는 괜찮은것 같고, 이동하면서 빼는 부분에서 오류가 발생하는것 같음..

--- CodingTest/ThroughBridge.cpp
+++ CodingTest/ThroughBridge.cpp
@@ -21,36 +21,19 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
 
 	//변수 및 추가 입력 값
 	vector<int> wait_on_bridge = {};
+	vector<int> through_bridge = {};
 	int sum_wait_on_bridge = 0;
-	vector<int> on_bridge = {};
 
+	//wait_on_bridge.size() = bridge_length()만큼 만들어줌
 	for (int i = 0; i < bridge_length; i++) {
-		on_bridge.push_back(0);
+		wait_on_bridge.push_back(0);
 	}
 
 	//!finished
 
 	while (!finished) {
-		//트럭이 땅->다리로 이동이 가능한지 확인
-		//트럭 1대가 추가로 이동이 가능한가?
-		if (bridge_length < wait_on_bridge.size() + 1)
-			overlength = true;
 
-		if (truck_weights.size() > 0)
-			remainOnGround = true;
-		else
-			remainOnGround = false;
-
-		if (wait_on_bridge.size() > 0)
-			remainOnBridge = true;
-		else
-			remainOnBridge = false;
-
-		//최종 조건: 땅과 다리 모든 트럭이 이동 했을때
-		if (!remainOnGround && !remainOnBridge) {
-			finished = true;
-			break;
-		}
+		
 
 		for (int i = 0; i < wait_on_bridge.size(); i++) 
 			sum_wait_on_bridge += wait_on_bridge[i];
@@ -60,11 +43,36 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
 				overweight = true;
 		}
 		else if (weight < sum_wait_on_bridge) {
-			if (weight < sum_wait_on_bridge)
+			if (weight < sum_wait_on_bridge) {
 				overweight = true;
+				//만약 overwait라서 트럭이 못올라간다면 빈칸 추가
+				wait_on_bridge.push_back(0);
+			}
 		}
 		else {
 			overweight = false;
+		}
+
+		//트럭이 땅->다리로 이동이 가능한지 확인
+		//트럭 1대가 추가로 이동이 가능한가?
+		if (bridge_length < wait_on_bridge.size())
+			overlength = true;
+
+		if (truck_weights.size() > 0)
+			remainOnGround = true;
+		else
+			remainOnGround = false;
+
+		//다리위에 트럭이 올라온 무게로 확인
+		if (sum_wait_on_bridge > 0)
+			remainOnBridge = true;
+		else
+			remainOnBridge = false;
+
+		//최종 조건: 땅과 다리 모든 트럭이 이동 했을때
+		if (!remainOnGround && !remainOnBridge) {
+			finished = true;
+			break;
 		}
 
 		//트럭이 땅에서 다리로 올라감
@@ -79,23 +87,27 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
 		if (remainOnBridge)
 		{
 			wait_on_bridge.erase(wait_on_bridge.begin());
+			if (wait_on_bridge.size()>0)
+			{
+				through_bridge.push_back(wait_on_bridge.front());
+			}
 		}
 
 		printf("%d | %d | %d\n", truck_weights.size(), wait_on_bridge.size(), seconds);
 		printf("%d | %d \n", remainOnGround, remainOnBridge);
 		printf("%d | %d \n\n", overweight, overlength);
-		printf("%d\n\n", on_bridge.size());
 
 		seconds++;
+
+		//초기화
 		sum_wait_on_bridge = 0;
-		finished = false;
 		overweight = false;
 		overlength = false;
 		remainOnBridge = false;
 		remainOnGround = false;
 	}
 
-	answer = seconds+bridge_length-1;
+	answer = seconds;
 	printf("Answer : %d ", answer);
 
 	system("pause");
@@ -103,5 +115,5 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
 }
 
 void main() {
-	solution(100, 100, { 10});
+	solution(4, 10, {1,9,7,3});
 }

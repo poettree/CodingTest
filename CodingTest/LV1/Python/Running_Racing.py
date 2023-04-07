0e7bc3f12cdf1078202e5de193cf446ab2f72765
poettree <61416449+poettree@users.noreply.github.com>
2023-04-07 23:06:31 +0900

    TIL-Day3-Python(Lv1)-NeedToFix-2

--- CodingTest/LV1/Python/Running_Racing.py
+++ CodingTest/LV1/Python/Running_Racing.py
@@ -1,21 +1,44 @@
 #information
-#Time = 0:20:13
-#Description: 정답은 맞지만, 느림
+#Time = 1:20:13
+#Description: 1번째 시도한 부분에서, Callings가 반복적으로 불리는 부분에 대하여, 전처리를 하고 진행하는 방식으로 했지만, 여전히 느림
+
 
 #Answer
 def solution(players, callings):
 #define
     answer = []
+    callingName = ''
+    callingTimes = 1
 
 #Pretreatment
 
 
 #Main
-    for playerName in range(len(callings)):         
+    while len(callings) > 0:
+        for times in range(len(callings)):
+            callingName = callings[times]
+            if(times < len(callings)-1):
+                if(callings[times] == callings[times+1]):
+                    callingTimes += 1
+                    print(callings[times])
+                elif(times+1 == len(callings)): 
+                    callingTimes += 1
+                    print(callings[times])
+                    break
+                else: break
+        del callings[0:callingTimes]
+
         for i in range(len(players)):
-            if(players[i] == callings[playerName] and i>0):
-                players.insert(i-1,callings[playerName])
-                players.pop(i+1)
+            if(players[i] == callingName):
+                players.insert(i-callingTimes,callingName)
+                print(i,":",callingTimes)
+                print(i,":",players)
+                if(callingTimes == 1):
+                    players.pop(i+callingTimes)
+                else: players.pop(i+callingTimes-1)
+                callingTimes = 1
+                print(i,":",players)
+                print(i,":",len(callings))
                 break
                 
     answer = players
